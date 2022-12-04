e612457d308318d34274fcf7c66ef0561c2e9297
Charlie <amante.c@northeastern.edu>
2022-12-04 15:49:38 -0500

    board commit 1

--- Tank.py
+++ Tank.py
@@ -121,7 +121,23 @@ class Wall:
 
 
 
+import pygame
+from pygame.locals import *
 
+pygame.init()
+screen_width = 800
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Tank Game")
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+pygame.quit()
 
 
 
